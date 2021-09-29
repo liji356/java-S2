bfcb95c214a322b4b464d8a7426d99e7af047fe5
liji356 <77767371+liji356@users.noreply.github.com>
2021-09-29 12:20:47 +0530

    Create face1.java

---
+++ face1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.applet.*;
+import java.awt.*;
+/*
+<Applet code="face1.java" width=500 ,height=600>
+</Applet>
+*/
+public class face1 extends Applet
+{
+public   void  paint(Graphics g)
+{
+g.drawOval(40,10,120,200);
+g.drawOval(57,75,30,20);
+g.drawRect(100,150,30,20);
+g.fillOval(60,70,35,40);
+}
+}
